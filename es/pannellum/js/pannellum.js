ea88c65b91f35eef24d86278abbd6336275845ec
Thomas <ek0onsec@protonmail.com>
2024-09-30 12:34:57 +0200

    add text under hotspot

--- es/pannellum/js/pannellum.js
+++ es/pannellum/js/pannellum.js
@@ -1697,89 +1697,104 @@ window.pannellum = function (window, document, undefined) {
       // Make sure hot spot pitch and yaw are numbers
       hs.pitch = Number(hs.pitch) || 0;
       hs.yaw = Number(hs.yaw) || 0;
-
+  
       var div = document.createElement('div');
       div.className = 'pnlm-hotspot-base';
-      if (hs.cssClass) {
-        div.className += ' ' + hs.cssClass;
-      } else {
-        div.className += ' pnlm-hotspot pnlm-sprite pnlm-' + escapeHTML(hs.type);
-      }
-
+      if (hs.cssClass)
+          div.className += ' ' + hs.cssClass;
+      else
+          div.className += ' pnlm-hotspot pnlm-sprite pnlm-' + escapeHTML(hs.type);
+  
       var span = document.createElement('span');
       if (hs.text) {
-        span.innerHTML = escapeHTML(hs.text);
+          span.innerHTML = escapeHTML(hs.text);
+  
+          // Créer un nouvel élément pour le texte
+          var textDiv = document.createElement('div');
+          textDiv.className = 'pnlm-hotspot-text';
+          textDiv.innerHTML = escapeHTML(hs.text);
+  
+          // Positionner le texte en dessous du point
+          textDiv.style.position = 'absolute';
+          textDiv.style.top = '100%';
+          textDiv.style.left = '50%';
+          textDiv.style.transform = 'translateX(-50%)';
+          textDiv.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+          textDiv.style.color = 'white';
+          textDiv.style.padding = '5px';
+          textDiv.style.borderRadius = '3px';
+          textDiv.style.whiteSpace = 'nowrap';
+  
+          div.appendChild(textDiv);
       }
-
+  
       var a;
       if (hs.video) {
-        var video = document.createElement('video'),
-            p = hs.video;
-        if (config.basePath && !absoluteURL(p)) {
-          p = config.basePath + p;
-        }
-        video.src = sanitizeURL(p);
-        video.controls = true;
-        video.style.width = hs.width + 'px';
-        renderContainer.appendChild(div);
-        span.appendChild(video);
+          var video = document.createElement('video'),
+              p = hs.video;
+          if (config.basePath && !absoluteURL(p))
+              p = config.basePath + p;
+          video.src = sanitizeURL(p);
+          video.controls = true;
+          video.style.width = hs.width + 'px';
+          renderContainer.appendChild(div);
+          span.appendChild(video);
       } else if (hs.image) {
-        var p = hs.image;
-        if (config.basePath && !absoluteURL(p)) {
-          p = config.basePath + p;
-        }
-        a = document.createElement('a');
-        a.href = sanitizeURL(hs.URL ? hs.URL : p);
-        a.target = '_blank';
-        span.appendChild(a);
-        var image = document.createElement('img');
-        image.src = sanitizeURL(p);
-        image.style.width = hs.width + 'px';
-        image.style.paddingTop = '5px';
-        renderContainer.appendChild(div);
-        a.appendChild(image);
-        span.style.maxWidth = 'initial';
+          var p = hs.image;
+          if (config.basePath && !absoluteURL(p))
+              p = config.basePath + p;
+          a = document.createElement('a');
+          a.href = sanitizeURL(hs.URL ? hs.URL : p);
+          a.target = '_blank';
+          span.appendChild(a);
+          var image = document.createElement('img');
+          image.src = sanitizeURL(p);
+          image.style.width = hs.width + 'px';
+          image.style.paddingTop = '5px';
+          renderContainer.appendChild(div);
+          a.appendChild(image);
+          span.style.maxWidth = 'initial';
       } else if (hs.URL) {
-        a = document.createElement('a');
-        a.href = sanitizeURL(hs.URL);
-        a.target = '_blank';
-        renderContainer.appendChild(a);
-        div.className += ' pnlm-pointer';
-        span.className += ' pnlm-pointer';
-        a.appendChild(div);
-      } else {
-        if (hs.sceneId) {
-          div.onclick = div.ontouchend = function () {
-            if (!div.clicked) {
-              div.clicked = true;
-              loadScene(hs.sceneId, hs.targetPitch, hs.targetYaw, hs.targetHfov);
-            }
-            return false;
-          };
+          a = document.createElement('a');
+          a.href = sanitizeURL(hs.URL);
+          a.target = '_blank';
+          renderContainer.appendChild(a);
           div.className += ' pnlm-pointer';
           span.className += ' pnlm-pointer';
-        }
-        renderContainer.appendChild(div);
+          a.appendChild(div);
+      } else {
+          if (hs.sceneId) {
+              div.onclick = div.ontouchend = function() {
+                  if (!div.clicked) {
+                      div.clicked = true;
+                      loadScene(hs.sceneId, hs.targetPitch, hs.targetYaw, hs.targetHfov, false);
+                  }
+                  return false;
+              };
+              div.className += ' pnlm-pointer';
+              span.className += ' pnlm-pointer';
+          }
+          renderContainer.appendChild(div);
       }
-
+  
       if (hs.createTooltipFunc) {
-        hs.createTooltipFunc(div, hs.createTooltipArgs);
+          hs.createTooltipFunc(div, hs.createTooltipArgs);
       } else if (hs.text || hs.video || hs.image) {
-        div.classList.add('pnlm-tooltip');
-        div.appendChild(span);
-        span.style.width = span.scrollWidth - 20 + 'px';
-        span.style.marginLeft = -(span.scrollWidth - div.offsetWidth) / 2 + 'px';
-        span.style.marginTop = -span.scrollHeight - 12 + 'px';
+          div.classList.add('pnlm-tooltip');
+          div.appendChild(span);
+          span.style.width = span.scrollWidth - 20 + 'px';
+          span.style.marginLeft = -(span.scrollWidth - div.offsetWidth) / 2 + 'px';
+          span.style.marginTop = -span.scrollHeight - 12 + 'px';
       }
       if (hs.clickHandlerFunc) {
-        div.addEventListener('click', function (e) {
-          hs.clickHandlerFunc(e, hs.clickHandlerArgs);
-        }, 'false');
-        div.className += ' pnlm-pointer';
-        span.className += ' pnlm-pointer';
+          div.addEventListener('click', function(e) {
+              hs.clickHandlerFunc(e, hs.clickHandlerArgs);
+          }, 'false');
+          div.className += ' pnlm-pointer';
+          span.className += ' pnlm-pointer';
       }
       hs.div = div;
-    };
+  }
 
     /**
     * Creates hot spot elements for the current scene.
